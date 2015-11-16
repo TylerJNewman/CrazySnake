a1d17d4a110f220cd9fe33a6948e5fb66b58d429
Tyler Newman <tylerjnewman18@gmail.com>
2015-11-15 18:48:36 -0800

    major changes

--- public/js/snake.js
+++ public/js/snake.js
@@ -1,174 +1,100 @@
-(function() {
-  var snakeGame = window.snakeGame = (window.snakeGame || {});
+(function () {
 
-  var Snake = snakeGame.Snake = function (board) {
-    this.turning = false;
-    this.direction = "E";
-    this.board = board;
-    this.growTurns = 0;
-    this.gameOver = false;
-    this.segments = [new Coord([10,10]), new Coord([10,11]), new Coord([10,12])];
+var Game = window.Game = window.Game || {};
 
-  };
+Game.Snake = function() {
+  this.headPos = new Coord(
+    Math.floor(Game.Config.boardWidth / 2),
+    Math.floor(Game.Config.boardHeight / 2)
+  );
+  this.currentDir = new Coord(1,0);
+  this.segments = [this.headPos, this.headPos.minus(this.currentDir), this.headPos.minus(this.currentDir).plus(this.currentDir)];
+};
 
-  Snake.prototype.moveDirection = function (direction) {
-    if (direction === "N") {
-      return new Coord([-1,0]);
-    }
-    if (direction === "E") {
-      return new Coord([0,1]);
-    }
-    if (direction === "S") {
-      return new Coord([1,0]);
-    }
-    if (direction === "W") {
-      return new Coord([0,-1]);
-    }
-  };
+var Snake = Game.Snake;
 
-  var Coord = snakeGame.Coord = function (coord) {
-    this.coord = coord;
-  };
+Game.Coord = function(row, col) {
+  this.pos = [row, col];
+};
 
-  var Board = snakeGame.Board = function () {
-    this.snake = new Snake(this);
-    this.apple = new Apple(this);
-  };
+var Coord = Game.Coord;
 
-  var Apple = snakeGame.Apple = function(board) {
-    this.board = board;
-    this.reset();
-  };
+Coord.prototype.eq = function (otherCoord) {
+  return this.pos[0] === otherCoord.pos[0] && this.pos[1] === otherCoord.pos[1];
+};
 
-  Apple.prototype.reset = function () {
-     var x = Math.floor(Math.random() * 20);
-     var y = Math.floor(Math.random() * 20);
+Coord.prototype.inverse = function (otherCoord) {
+  return this.pos[0] === -1 * otherCoord.pos[0] && this.pos[1] === - 1 * otherCoord.pos[1];
+};
 
-     while (this.board.snake.isOccupying([x, y])) {
-       x = Math.floor(Math.random() * 20);
-       y = Math.floor(Math.random() * 20);
-     }
+Coord.prototype.minus = function (otherCoord) {
+  return new Coord(this.pos[0] - otherCoord.pos[0], this.pos[1] - otherCoord.pos[1]);
+};
 
-     this.position = new Coord([x, y]);
+Coord.prototype.plus = function (otherCoord) {
+  return new Coord(this.pos[0] + otherCoord.pos[0], this.pos[1] + otherCoord.pos[1]);
+};
 
-   };
+//
+Snake.prototype.changeDir = function (dir) {
+  if (!dir.inverse(this.currentDir) && this.validMove(dir)) {
+    this.currentDir = new Coord(dir.pos[0], dir.pos[1]);
+  }
+};
 
-   Snake.prototype.eatApple = function () {
-     if (this.head().equals(this.board.apple.position)) {
-       this.growTurns += 1;
-       return true;
-     } else {
-       return false;
-     }
-   };
+Snake.prototype.eat = function () {
+  this.eating = true;
+};
 
-   Snake.prototype.head = function () {
-     return this.segments[this.segments.length - 1];
-   };
+Snake.prototype.eatingSelf = function () {
+  for (var i = 1; i < this.length(); i++) {
+    if (this.segments[i].eq(this.headPos)) return true;
+  }
 
-  Snake.prototype.isValid = function () {
+  return false;
+};
 
-    var head = this.head();
+Snake.prototype.length = function () {
+  return this.segments.length;
+};
 
-    if (!this.board.validPosition(this.head())) {
-      return false;
-    }
+Snake.prototype.move = function () {
+  // only move if queue does not send us off the wall
+  if (this.validMove(this.currentDir)) {
+    this.headPos = this.headPos.plus(this.currentDir);
+  } else {
+    return;
+  }
 
-    for (var i = 0; i < this.segments.length - 1; i++) {
-      if (this.segments[i].equals(head)) {
-        return false;
-      }
-    }
+  this.segments.unshift(this.headPos);
+  if (!this.eating) {
+    this.segments.pop();
+  } else {
+  }
+  this.eating = false;
+};
 
-    return true;
-  };
+Snake.prototype.on = function (coord) {
+  for (var i = 0; i < this.length(); i++) {
+    if (this.segments[i].eq(coord)) return true;
+  }
 
+  return false;
+};
 
-   Snake.prototype.isOccupying = function (array) {
-      var result = false;
-      this.segments.forEach(function (segment) {
-        if (segment.coord[0] === array[0] && segment.coord[1] === array[1]) {
-          result = true;
-          return result;
-        }
-      });
-      return result;
-    };
+Snake.prototype.validMove = function (dir) {
+  var nextPos = this.headPos.plus(dir);
+  return (!this.offBoard(nextPos));
+};
 
-  Snake.prototype.getSegments = function () {
-    var segs = [];
-    for (var i = 0; i < this.segments.length; i++) {
-      segs.push(this.segments[i].coord);
-    }
-    return segs;
-  };
+Snake.prototype.offBoard = function (coord) {
+  return coord.pos[0] < 0 || coord.pos[1] < 0 ||
+  coord.pos[0] >= Game.Config.boardWidth ||
+  coord.pos[1] >= Game.Config.boardHeight;
+};
 
-
-  Coord.prototype.plus = function (position) {
-    return [this.coord[0] + position.coord[0], this.coord[1] + position.coord[1]];
-  };
-
-  Coord.prototype.equals = function (position) {
-
-    return (this.coord[0] === position.coord[0]) && (this.coord[1] === position.coord[1]);
-  };
-
-  Coord.prototype.isOpposite = function (position) {
-
-    return (this.coord.coord[0] === ((position.coord.coord[0])*-1)) || (this.coord.coord[1] === ((position.coord.coord[1])*-1));
-  };
-
-  Snake.prototype.move = function () {
-
-
-
-    var dir = this.moveDirection(this.direction);
-    var last = this.segments.length - 1;
-    this.turning = false;
-    var newcoord = this.segments[last].plus(dir);
-    this.segments.push(new Coord(newcoord));
-    if (this.eatApple()) {
-        this.board.apple.reset();
-
-      }
-
-    if (this.growTurns > 0) {
-      this.growTurns -= 1;
-    } else {
-      this.segments.shift();
-    }
-    if (!this.isValid()) {
-         this.segments = [];
-    }
-
-
-
-
-  };
-
-  Snake.prototype.turn = function (dir) {
-    var x = new Coord(this.moveDirection(this.direction));
-    var y = new Coord(this.moveDirection(dir));
-    if(x.isOpposite(y) || this.turning){
-      return;
-    } else {
-      this.turning = true;
-      this.direction = dir;
-    }
-
-    if (!this.isValid()) {
-        this.gameOver = true;
-    }
-
-  };
-
-  Board.prototype.validPosition = function (coord) {
-
-      return (coord.coord[0] >= 0) && (coord.coord[0] < 20) &&
-        (coord.coord[1] >= 0) && (coord.coord[1] < 20);
-  };
-
-  // var test = new Board();
-  // test.render();
+// Snake.prototype.nextHeadPos = function () {
+//   return this.headPos.plus(this.currentDir);
+// };
 
 })();
